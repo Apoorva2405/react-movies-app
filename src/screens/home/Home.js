2bb4c1c7894e18f3aebab0bd5be0a464660d1468
Apoorva2405 <apsharma@adobe.com>
2018-10-20 11:32:19 +0530

    Added 'Movie Name' filter on home page

--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -6,31 +6,60 @@ import moviesData from '../../assets/movieData';
 import GridList from '@material-ui/core/GridList';
 import GridListTile from '@material-ui/core/GridListTile';
 import GridListTileBar from '@material-ui/core/GridListTileBar';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import FormControl from '@material-ui/core/FormControl';
+import Typography from '@material-ui/core/Typography';
+import InputLabel from '@material-ui/core/InputLabel';
+import Input from '@material-ui/core/Input';
 
 
 const styles = theme => ({
-   root: {
-       flexGrow: 1,
-       backgroundColor: theme.palette.background.paper
-   },
-   upcomingMoviesHeading: {
-       textAlign: 'center',
-       background: '#ff9999',
-       padding: '8px',
-       fontSize: '1rem'
-   },
-   gridListUpcomingMovies: {
-       flexWrap: 'nowrap',
-       transform: 'translateZ(0)',
-       width: '100%'
-   }
+    root: {
+        flexGrow: 1,
+        backgroundColor: theme.palette.background.paper
+    },
+    upcomingMoviesHeading: {
+        textAlign: 'center',
+        background: '#ff9999',
+        padding: '8px',
+        fontSize: '1rem'
+    },
+    gridListUpcomingMovies: {
+        flexWrap: 'nowrap',
+        transform: 'translateZ(0)',
+        width: '100%'
+    },
+    gridListMain: {
+        transform: 'translateZ(0)',
+        cursor: 'pointer'
+    },
+    formControl: {
+        margin: theme.spacing.unit,
+        minWidth: 240,
+        maxWidth: 240
+    },
+    title: {
+        color: theme.palette.primary.light,
+    }
 });
 
 
 class Home extends Component {
 
+    constructor() {
+        super();
+        this.state = {
+            movieName: ""
+        }
+    }
+    movieNameChangeHandler = event => {
+        this.setState({ movieName: event.target.value });
+    }
+
     render() {
-const { classes } = this.props;
+        const { classes } = this.props;
         return (
             <div>
                 <Header />
@@ -39,12 +68,42 @@ const { classes } = this.props;
                 </div>
                 <GridList cols={5} className={classes.gridListUpcomingMovies}>
                     {moviesData.map(movie => (
-                        <GridListTile key = {movie.id}>
-                            <img src={movie.poster_url} alt={movie.title} />
+                        <GridListTile key={movie.id}>
+                            <img src={movie.poster_url} alt={movie.title} className="movie-poster" />
                             <GridListTileBar title={movie.title} />
                         </GridListTile>
                     ))}
                 </GridList>
+                <div className="flex-container">
+                    <div className="left">
+                        <GridList cellHeight={350} cols={4} className={classes.gridListMain}>
+                            {moviesData.map(movie => (
+                                <GridListTile className="released-movie-grid-item" key={"grid" + movie.id}>
+                                    <img src={movie.poster_url} className="movie-poster" alt={movie.title} />
+                                    <GridListTileBar
+                                        title={movie.title}
+                                        subtitle={<span>Release Date: {new Date(movie.release_date).toDateString()}</span>}
+                                    />
+                                </GridListTile>
+                            ))}
+                        </GridList>
+                    </div>
+                    <div className="right">
+                        <Card>
+                            <CardContent>
+                                <FormControl className={classes.formControl} >
+                                    <Typography className={classes.title} color="textSecondary">
+                                        FIND MOVIES BY:
+                        </Typography>
+                                </FormControl>
+                                <FormControl className={classes.formControl}>
+                                    <InputLabel htmlFor="movieName">Movie Name</InputLabel>
+                                    <Input id="movieName" onChange={this.movieNameChangeHandler} />
+                                </FormControl>
+                            </CardContent>
+                        </Card>
+                    </div>
+                </div>
             </div>
         )
     }
